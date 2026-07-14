fd26b2c866bf885fec406351243b27d96797f1d6
Kabir Suri <r.surii1309@gmail.com>
2026-07-14 15:49:34 +0100

    pejlngerl

--- Conditionals/Pole.cs
+++ Conditionals/Pole.cs
@@ -2,8 +2,27 @@ namespace Conditionals
 {
     class program
     {
-        static void Main(string[] args)} 
-}
+        static void Main(string[] args) {
+            Pole pole = new Pole.North;
+            string animal;
+
+            if (pole == Pole.North)
+            {
+                animal = "Polar Bear"
+            }
+            else
+            {
+                animal = "Pengu"
+            }
+
+        Console.WriteLine($"The animal that lives in the {pole} Pole is the {animal}")
+
+
+
+
+
+        }
+    }
     public enum Pole
     {
         North,
