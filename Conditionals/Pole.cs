86b40ae6a1f174f563120591882c94d7de671033
Frankzyy128 <francotang2000@gmail.com>
2026-07-14 16:23:01 +0100

    Refactor Pole enum and add capital cities switch
    Refactored the Pole enum and added a switch statement for capital cities.

--- Conditionals/Pole.cs
+++ Conditionals/Pole.cs
@@ -2,8 +2,22 @@ namespace Conditionals
 {
     class program
     {
-        static void Main(string[] args) {
-            Pole pole = new Pole.North;
+        public enum Pole
+        {
+            North,
+            South
+        }
+        enum CapitalCities
+        {
+            London,
+            Paris,
+            Madrid,
+            Rome
+        }
+
+        static void Main(string[] args)
+        {
+            Pole pole = Pole.North;
             string animal;
 
             if (pole == Pole.North)
@@ -12,17 +26,45 @@ namespace Conditionals
             }
             else
             {
-                animal = "Pengu";
+                animal = "Penguin";
             }
 
             Console.WriteLine($"The animal that lives in the {pole} Pole is the {animal}");
 
-                
-        }
+            pole = Pole.South;
+            animal = (pole == Pole.North) ? "Polar Bear" : "Penguin";
+
+            Console.WriteLine($"The animal that lives in the {pole} Pole is the {animal}");
+
+
+
+
+            Console.WriteLine("##### Switch Statement #####");
+            var city = CapitalCities.Madrid;
+            string countryMessage = "";
+
+            switch (city) 
+            {
+                case CapitalCities.London:
+                    countryMessage = $"{city} is the capital of the UK";
+                    break;
+
+                case CapitalCities.Paris:
+                    countryMessage = $"{city} is the capital of France";
+                    break;
+
+                case CapitalCities.Rome:
+                    countryMessage = $"{city} is the capital of Italy";
+                    break;
+
+                case CapitalCities.Madrid:
+                    countryMessage = $"{city} is the capital of Spain";
+                    break;
+
+                default:
+                    countryMessage = "Unknown city";
+                    break;
+            }
     }
-    public enum Pole
-    {
-        North,
-        South
-    }
-}
+ 
+}}
